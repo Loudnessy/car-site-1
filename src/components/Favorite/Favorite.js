34bb3cdbf162d96ae432563922629136c462a3e4
Eldar <eldarnaumenko2006@gmail.com>
2024-01-22 23:45:19 +0200

    hw

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,44 +1,51 @@
-import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { getFavoriteCars } from "api/api"
-import { CarCard } from "components/CarCard/CarCard"
-import { StyledContainer, StyledUl } from "components/Catalog/Catalog.styled"
-import ReactModal from "react-modal"
-import { Modal } from "components/Modal/Modal"
-import { changeChoosenCar } from "redux/slices/carsSlice"
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getFavoriteCars } from 'api/api';
+import { CarCard } from 'components/CarCard/CarCard';
+import { StyledContainer, StyledUl } from 'components/Catalog/Catalog.styled';
+import ReactModal from 'react-modal';
+import { Modal } from 'components/Modal/Modal';
+import { changeChoosenCar } from 'redux/slices/carsSlice';
+import { StyledParagraph } from './Favorite.styled';
 
 const Favorite = () => {
-    const dispatch = useDispatch()
-    const favorite = useSelector(state => state.cars.favorite)
-    const cars = useSelector(state => state.cars.items);
-    const closeModal = () => {
-        setModalIsOpen(false);
-        dispatch(changeChoosenCar(''));
-      };
-    useEffect(() => {
-        ReactModal.setAppElement('#main');
-        if (favorite.length) {
-        dispatch(getFavoriteCars(favorite))    
-        }
-    }, [favorite, dispatch])
-    const [modalIsOpen, setModalIsOpen] = useState(false);    
-return <main>
-    <section>
-   <StyledContainer id="main">
-{favorite.length? <StyledUl>
-    {cars.map(car => {
-              return (
-                <CarCard
-                  car={car}
-                  key={car.id}
-                  modal={setModalIsOpen}
-                ></CarCard>
-              );
-            })}
-</StyledUl> : <p>you dont have any favorite cars</p>}
-<Modal modalIsOpen={modalIsOpen} closeModal={closeModal}/>
-   </StyledContainer>
-    </section>
-</main>
-}
-export default Favorite
+  const dispatch = useDispatch();
+  const favorite = useSelector(state => state.cars.favorite);
+  const cars = useSelector(state => state.cars.items);
+  const closeModal = () => {
+    setModalIsOpen(false);
+    dispatch(changeChoosenCar(''));
+  };
+  useEffect(() => {
+    ReactModal.setAppElement('#main');
+    if (favorite.length) {
+      dispatch(getFavoriteCars(favorite));
+    }
+  }, [favorite, dispatch]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  return (
+    <main>
+      <section>
+        <StyledContainer id="main">
+          {favorite.length ? (
+            <StyledUl>
+              {cars.map(car => {
+                return (
+                  <CarCard
+                    car={car}
+                    key={car.id}
+                    modal={setModalIsOpen}
+                  ></CarCard>
+                );
+              })}
+            </StyledUl>
+          ) : (
+            <StyledParagraph>You dont have any favorite cars</StyledParagraph>
+          )}
+          <Modal modalIsOpen={modalIsOpen} closeModal={closeModal} />
+        </StyledContainer>
+      </section>
+    </main>
+  );
+};
+export default Favorite;
--- src/components/Favorite/Favorite.styled.js
+++ src/components/Favorite/Favorite.styled.js
@@ -1,8 +1,18 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 export const StyledContainer = styled.div`
   @media screen and (min-width: 1440px) {
     margin: 0 auto;
     width: 1214px;
     padding: 0 15px;
   }
-`
+`;
+export const StyledParagraph = styled.p`
+  color: #121417;
+
+  font-family: Manrope;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: 1.33; /* 133.333% */
+  text-align: center;
+`;
